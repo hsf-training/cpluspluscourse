41f7d3a0b42e47291479117f87da6c1f0fe65f37
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2014-02-14 16:33:38 +0100

    Lot's of new tests around tools and few slides

---
+++ code/cppcheck/test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+#define LEN 1000
+#define STEP 10
+
+void swap(int *a, int*b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void randomize(int* v, unsigned int len) {
+    // we randomize via len random inversions
+    for (unsigned int i = 0; i < len; i++) {
+        int a = rand()%len;
+        int b = rand()%len;
+        swap(v+a, v+b);
+    }
+}
+
+void fillVector(int* v, unsigned int len) {
+    for (unsigned int i = 0; i < len; i++) v[i] = i*STEP;
+}
+
+int main() {
+    int v[LEN-1];
+    // create and randomize vector
+    fillVector(v, LEN+1);
+    randomize(v, LEN+1);
+
+    // compute diffs
+    int diffs[LEN];
+    for (unsigned int i = 0; i < LEN; i++)
+        diffs[i] = v[i+1] - v[i];
+
+    // compute standard deviation of it
+    float sum = 0;
+    float sumsq = 0;
+    for (unsigned int i = 0; i < LEN; i ++) {
+        sum += diffs[i];
+        sumsq += diffs[i]*diffs[i];
+    }
+    float mean = sum/LEN;
+    float stddev = sqrt(sumsq/LEN - mean*mean) ;
+    std::cout << "Range = [0, " << STEP*LEN << "]\n"
+              << "Mean = " << mean
+              << "\nStdDev = " << stddev << std::endl;
+}
---
+++ code/memcheck/Polygons.hpp
@@ -0,0 +1,18 @@
+class Polygon {
+public:
+    Polygon(int n, float radius);
+    ~Polygon(){};
+    float computePerimeter();
+protected:
+    int m_nbSides;
+    int m_radius;
+};
+
+class Hexagon : public Polygon {
+public:
+    Hexagon(char* name, float radius);
+    ~Hexagon();
+    char* name() const {return m_name;};
+private:
+    char* m_name;
+};
