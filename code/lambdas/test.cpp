b25c988526ace6afd19d5f8d8c2ce4aa5e4e6101
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2014-02-18 14:05:15 +0100

    Added lambda exercise

---
+++ code/lambdas/test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <ext/numeric>
+#include "Complex.hpp"
+
+using namespace std;
+using namespace __gnu_cxx;
+
+template<class T>
+struct Generator {
+    T m_value, m_step;
+    Generator(T initial, T step):m_value(initial-step), m_step(step){};
+    T operator()() {
+        T cur_value = m_value;
+        m_value += m_step;
+        return cur_value;
+    }
+};
+
+template<class T>
+struct sumsquare {
+    T operator()(T& s, T& a) { return s + a * a; };
+};
+    
+template<class T>
+void compute(int len, T initial, T step) {
+    // allocate vectors
+    std::vector<T> v(len+1), diffs(len+1);
+
+    // fill and randomize v
+    generate(v.begin(), v.end(), Generator<T>(initial, step));
+    random_shuffle(v.begin(), v.end());
+    
+    // compute differences
+    adjacent_difference(v.begin(), v.end(), diffs.begin());    
+
+    // compute standard deviation of it
+    T sum = accumulate(diffs.begin()+1, diffs.end(), T());
+    T sumsq = accumulate(diffs.begin()+1, diffs.end(), T(), sumsquare<T>());
+    T mean = sum/len;
+    T variance = sumsq/len - mean*mean;
+
+    std::cout << "Range = [" << initial << ", " << step*len << "]\n"
+              << "Mean = " << mean << "\n"
+              << "Variance = " << variance << std::endl;
+}
+
+int main() {
+    compute(1000, 0.0, 7.0);
+    compute(1000, Complex(0,0), Complex(1,2));
+}
---
+++ code/lambdas/test.sol.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <ext/numeric>
+#include "Complex.hpp"
+
+using namespace std;
+using namespace __gnu_cxx;
+
+template<class T>
+void compute(int len, T initial, T step) {
+    // allocate vectors
+    std::vector<T> v(len+1), diffs(len+1);
+
+    // fill and randomize v
+    T current = initial;
+    generate(v.begin(), v.end(),
+             [&current, step]() {T cur = current;
+                                 current += step;
+                                 return cur;});
+    random_shuffle(v.begin(), v.end());
+    
+    // compute differences
+    adjacent_difference(v.begin(), v.end(), diffs.begin());    
+
+    // compute standard deviation of it
+    T sum = accumulate(diffs.begin()+1, diffs.end(), T());
+    T sumsq = accumulate(diffs.begin()+1, diffs.end(), T(),
+                         [](T& s, T& a){ return s + a * a;});
+    T mean = sum/len;
+    T variance = sumsq/len - mean*mean;
+
+    std::cout << "Range = [" << initial << ", " << step*len << "]\n"
+              << "Mean = " << mean << "\n"
+              << "Variance = " << variance << std::endl;
+}
+
+int main() {
+    compute(1000, 0.0, 7.0);
+    compute(1000, Complex(0,0), Complex(1,2));
+}
