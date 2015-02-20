7483ebd80ed4c0bde033391a32ef650147503821
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2015-02-20 22:14:20 +0100

    Added a code example for race conditions

---
+++ code/race/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+
+int a = 0;
+
+void inc() {
+  a++;
+}
+
+void inc100() {
+  for (int i = 0; i < 100; i++)
+    inc();
+}
+
+int main() {
+  for (int j = 0; j < 1000; j++) {
+    a = 0;
+    std::thread t1(inc100);
+    std::thread t2(inc100);
+    for (auto t: {&t1,&t2}) t->join();
+    if (a != 200) {
+      std::cout << a;
+    } else {
+      std::cout << '.';
+    }
+  }
+  std::cout << std::endl;
+}
+
