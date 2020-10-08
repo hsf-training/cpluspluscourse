cb6d265c2832c3dc8e02079414d2e3f55675504e
David Chamont <chamont@in2p3.fr>
2020-10-08 10:03:38 +0200

    Take hello example from the course.

--- code/hello/hello.cpp
+++ code/hello/hello.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+
+// This is a function
+void print(int i) {
+  std::cout << "Hello, world " << i << std::endl;
+}
+
 int main() {
-    std::cout<<"Hello World !"<<std::endl ;
+  int n = 3;
+  for (int i = 0; i < n; i++) {
+    print(i);
+  }
+  return 0;
 }
