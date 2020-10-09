7c33424f839a46e4dec4360d167a25a79d34d3c3
David Chamont <chamont@in2p3.fr>
2020-10-09 11:18:32 +0200

    New option needed so that gdb can switch off space randomization.

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 SCRIPT_NAME=${BASH_SOURCE[0]}
 SCRIPT_DIR=`dirname ${SCRIPT_NAME}`
 
-docker run -it --rm -v $PWD:/work -w /work `cat ${SCRIPT_DIR}/name.txt` $*
+docker run --security-opt seccomp=unconfined -it --rm -v $PWD:/work -w /work `cat ${SCRIPT_DIR}/name.txt` $*
--- docker/run_x11_macos.sh
+++ docker/run_x11_macos.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=`dirname ${SCRIPT_NAME}`
 export DISPLAY=${MYIP}:0
 xhost + ${MYIP}
 
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw -it --rm -v $PWD:/work -w /work `cat ${SCRIPT_DIR}/name.txt` $*
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw --security-opt seccomp=unconfined -it --rm -v $PWD:/work -w /work `cat ${SCRIPT_DIR}/name.txt` $*
