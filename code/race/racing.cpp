2dbc2ada738d9b25eb703c05baf446fa6c01fc77
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2022-01-12 15:52:20 +0100

    [concurrency] Work on mutex
    - Remove global variables from mutex example, use lambdas.
    
    In order to not show using globals, I thought using lambdas to capture
    local variables is cleaner.
    I moved to C++17, since we say in the lecture now that people should
    alwyas use scoped_lock.
    
    I also return an exit status that captures whether or not there was a
    race.

--- code/race/racing.cpp
+++ code/race/racing.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 #include <thread>
 
-int a = 0;
-
-void inc() {
-  a++;
-}
-
-void inc100() {
-  for (int i = 0; i < 100; i++)
-    inc();
-}
+/*
+ * This program tries to increment an integer 200 times in two threads.
+ * Check whether the result is indeed always 200.
+ */
 
 int main() {
+  int nError = 0;
+
   for (int j = 0; j < 1000; j++) {
-    a = 0;
+    int a = 0;
+
+    // Increment the variable a 100 times:
+    auto inc100 = [&a](){
+      for (int i = 0; i < 100; ++i) {
+        a++;
+      }
+    };
+
+    // Run with two threads
     std::thread t1(inc100);
     std::thread t2(inc100);
-    for (auto t: {&t1,&t2}) t->join();
+    for (auto t : {&t1,&t2}) t->join();
+
+    // Check
     if (a != 200) {
-      std::cout << a;
+      std::cout << "Race: " << a << ' ';
+      nError++;
     } else {
       std::cout << '.';
     }
   }
   std::cout << '\n';
+
+  return nError;
 }
--- code/race/solution/racing.sol.cpp
+++ code/race/solution/racing.sol.cpp
@@ -2,30 +2,40 @@
 #include <thread>
 #include <mutex>
 
-int a = 0;
-std::mutex m;
-
-void inc() {
-  std::lock_guard<std::mutex> guard(m);
-  a++;
-}
-
-void inc100() {
-  for (int i = 0; i < 100; i++)
-    inc();
-}
+/*
+ * This program tries to increment an integer 200 times in two threads.
+ * We fix the race condition by locking a mutex before each increment.
+ */
 
 int main() {
+  int nError = 0;
+
   for (int j = 0; j < 1000; j++) {
-    a = 0;
+    int a = 0;
+    std::mutex aMutex;
+
+    // Increment the variable a 100 times:
+    auto inc100 = [&a,&aMutex](){
+      for (int i = 0; i < 100; ++i) {
+        std::scoped_lock lock{aMutex};
+        a++;
+      }
+    };
+
+    // Run with two threads
     std::thread t1(inc100);
     std::thread t2(inc100);
-    for (auto t: {&t1,&t2}) t->join();
+    for (auto t : {&t1,&t2}) t->join();
+
+    // Check
     if (a != 200) {
-      std::cout << a;
+      std::cout << "Race: " << a << ' ';
+      nError++;
     } else {
       std::cout << '.';
     }
   }
   std::cout << '\n';
+
+  return nError;
 }
