186a2b606e0f6a340e999cd8d9be57fb7f21230e
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2022-02-28 12:17:11 +0100

    Adapt operators.sol to changes in original.

--- code/operators/solution/operators.sol.cpp
+++ code/operators/solution/operators.sol.cpp
@@ -82,6 +82,16 @@ bool operator<=(Fraction a, Fraction b) { return !(a > b); }
 
 bool operator>=(Fraction a, Fraction b) { return !(a < b); }
 
+
+void printAndCheck(const std::string & what, const Fraction & result, const Fraction & expected) {
+  const bool passed = result == expected;
+  std::cout << std::left << std::setw(40) << what << ": " << (passed ? "PASS" : "** FAIL **") << "    " << result << "\n";
+}
+void printAndCheck(const std::string & what, bool result, bool expected) {
+  const bool passed = result == expected;
+  std::cout << std::left << std::setw(40) << what << ": " << (passed ? "PASS" : "** FAIL **") << "    " << result << "\n";
+}
+
 int main() {
   // create a fraction with values 3 (which is 3/1) and 1/3
   const Fraction three{3};
@@ -92,42 +102,43 @@ int main() {
   std::cout << "One third: " << athird << '\n';
 
   // multiply fraction with an int
-  std::cout << "One third times two: " << athird * 2 << '\n';
+  // the printAndCheck function requires operator<< and operator==:
+  printAndCheck("One third times two", athird * 2, Fraction{2, 3});
   // ensure symmetry
-  std::cout << "One third times two: " << 2 * athird << '\n';
+  printAndCheck("One third times two", 2 * athird, Fraction{2, 3});
 
   // multiply two fractions
-  std::cout << "Three times one third: " << three * athird << '\n';
+  printAndCheck("Three times one third", three * athird, Fraction{1, 1});
   // normalize the fraction after multiplication so the above statement
   // prints 1/1 instead of e.g. 3/3
   // you might need to compute the greatest common divisor, for which you can
   // use the function std::gcd(a, b) from the <numeric> header
-  std::cout << "Three times one third: " << 3 * athird << '\n';
+  printAndCheck("Three times one third", 3 * athird, Fraction{1, 1});
 
   // multiply in place
   Fraction f = athird;
   f *= 2;
-  std::cout << "One third times two: " << f << '\n';
+  printAndCheck("One third times two", f, Fraction{2, 3});
 
   f = athird;
   f *= athird;
-  std::cout << "One third times one third: " << f << '\n';
+  printAndCheck("One third times one third", f, Fraction{1, 9});
 
   f = athird;
   f *= f *= f;
-  std::cout << "One third times itself twice: " << f << '\n';
+  printAndCheck("One third times itself twice", f, Fraction{1, 81});
 
   // you might have some redundancy between the implementation of operator* and
   // operator*=. Can you refactor your code and implement operator* in terms of
   // operator*=?
 
   // add an int to a fraction
-  std::cout << "One third plus 2: " << athird + 2 << '\n';
+  printAndCheck("One third plus 2", athird + 2, Fraction{7, 3});
   // ensure symmetry
-  std::cout << "2 plus one third: " << 2 + athird << '\n';
+  printAndCheck("2 plus one third", 2 + athird, Fraction{7, 3});
 
   // add two fractions
-  std::cout << "One third plus two sixth: " << athird + Fraction{2, 6} << '\n';
+  printAndCheck("One third plus two sixth", athird + Fraction{2, 6}, Fraction{2, 3});
   // normalize the fraction after addition too so the above statement
   // prints 2/3 instead of 12/18
   // make sure the normalization between addition and multiplication is not
@@ -136,31 +147,28 @@ int main() {
   // add in place
   f = athird;
   f += f += 1;
-  std::cout << "One third plus one and added to itself: " << f << '\n';
+  printAndCheck("One third plus one and added to itself", f, Fraction{8, 3});
   // again, try to refactor and implement operator+ in terms of operator+=
 
   std::cout << std::boolalpha;  // print bools as 'true' or 'false' from now on
 
-  // compare two fractions for equality, all of these should be 'true'
-  std::cout << "One third == one third: " << (athird == Fraction{1, 3}) << '\n'
-            << "One third != one forth: " << (athird != Fraction{1, 4}) << '\n'
-            << "One third == two sixth: " << (athird == Fraction{2, 6}) << '\n'
-            << "One third != three sixth: " << (athird != Fraction{3, 6})
-            << '\n';
+  // more equality comparisons
+  printAndCheck("One third == one third", (athird == Fraction{1, 3}), true);
+  printAndCheck("One third != one forth", (athird != Fraction{1, 4}), true);
+  printAndCheck("One third == two sixth", (athird == Fraction{2, 6}), true);
+  printAndCheck("One third != three sixth", (athird != Fraction{3, 6}), true);
   // try to implement operator!= in terms of operator==
 
   // more comparisons
   const Fraction afourth{1, 4};
-  std::cout << athird << " < " << athird << " : " << (athird < athird) << '\n'
-            << afourth << " < " << athird << " : " << (afourth < athird) << '\n'
-            << athird << " <= " << athird << ": " << (athird <= athird) << '\n'
-            << athird << " <= " << afourth << ": " << (athird <= afourth)
-            << '\n'
-            << athird << " > " << athird << ": " << (athird > athird) << '\n'
-            << afourth << " > " << athird << ": " << (afourth > athird) << '\n'
-            << athird << " >= " << athird << ": " << (athird >= athird) << '\n'
-            << athird << " >= " << afourth << ": " << (athird >= afourth)
-            << '\n';
+  printAndCheck("athird < athird", (athird < athird), false);
+  printAndCheck("afourth < athird", (afourth < athird), true);
+  printAndCheck("athird  <= athird", (athird <= athird), true);
+  printAndCheck("athird  <= afourth", (athird <= afourth), false);
+  printAndCheck("athird  > athird", (athird > athird), false);
+  printAndCheck("afourth > athird", (afourth > athird), false);
+  printAndCheck("athird >= athird", (athird >= athird), true);
+  printAndCheck("athird >= afourth", (athird >= afourth), true);
   // the operators <=, >= and > can typically be implemented just in terms of
   // operator<. Can you do this as well? ;)
 
@@ -168,32 +176,5 @@ int main() {
   // * we can very often implement an arithemtic operator@ in terms of
   // operator@=
   // * it usually suffices to implement operator< and operator== and derive the
-  // other relational operators from them. C++20 will do this automatically.
+  // other relational operators from them. C++20 will do part of this automatically.
 }
-
-// The final output of your program should be along these lines:
-// Three: 3/1
-// One third: 1/3
-// One third times two: 2/3
-// One third times two: 2/3
-// Three times one third: 1/1
-// Three times one third: 1/1
-// One third times two: 2/3
-// One third times one third: 1/9
-// One third times itself twice: 1/81
-// One third plus 2: 7/3
-// 2 plus one third: 7/3
-// One third plus two sixth: 2/3
-// One third plus one and added to itself: 8/3
-// One third == one third: true
-// One third != one forth: true
-// One third == two sixth: true
-// One third != three sixth: true
-// 1/3 < 1/3 : false
-// 1/4 < 1/3 : true
-// 1/3 <= 1/3: true
-// 1/3 <= 1/4: false
-// 1/3 > 1/3: false
-// 1/4 > 1/3: false
-// 1/3 >= 1/3: true
-// 1/3 >= 1/4: true
