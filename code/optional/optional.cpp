707a84e9572a805a6d24c38a1dcda552770148ff
David Chamont <chamont@in2p3.fr>
2022-10-05 23:30:26 +0200

    An exercise for std::optional (#303)
    Partly fixes #292
    
    Co-authored-by: Sebastien Ponce <sebastien.ponce_github@m4x.org>

---
+++ code/optional/optional.cpp
@@ -0,0 +1,37 @@
+
+/*
+
+In the code below, modify the function `mysqrt` below so that it returns
+an `std::optional<double>` rather than a double, and the program prints
+`nothing` rather than `nan` for the call with `-10`. It will also require
+to modify `square`.
+
+*/
+
+#include <cmath>
+#include <iostream>
+#include <optional>
+
+double mysqrt(double d) // TO BE MODIFIED
+{
+  return std::sqrt(d);  // TO BE MODIFIED
+}
+
+double square(double d) // TO BE MODIFIED
+{
+  return d * d;         // TO BE MODIFIED
+}
+
+template <typename A>
+std::ostream &operator<<(std::ostream &os, std::optional<A> const &opt) {
+  if (opt) {
+    return os << opt.value();
+  } else {
+    return os << "nothing";
+  }
+}
+
+int main() {
+  std::cout << square(mysqrt(10)) << std::endl;
+  std::cout << square(mysqrt(-10)) << std::endl;
+}
