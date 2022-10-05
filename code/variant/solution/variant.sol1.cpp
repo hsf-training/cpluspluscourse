66cac3dad6af9cfa692f421defe1aa70918f0254
David Chamont <chamont@in2p3.fr>
2022-10-05 17:23:14 +0200

    clang format

--- code/variant/solution/variant.sol1.cpp
+++ code/variant/solution/variant.sol1.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <variant>
+#include <vector>
 
-struct Electron
- { void print() const { std::cout<<"E"<<std::endl ; } } ;
+struct Electron {
+  void print() const { std::cout << "E" << std::endl; }
+};
 
-struct Proton
- { void print() const{ std::cout<<"P"<<std::endl ; } } ;
+struct Proton {
+  void print() const { std::cout << "P" << std::endl; }
+};
 
-struct Neutron
- { void print() const{ std::cout<<"N"<<std::endl ; } } ;
+struct Neutron {
+  void print() const { std::cout << "N" << std::endl; }
+};
 
-using Particle = std::variant<Electron,Proton,Neutron> ;
+using Particle = std::variant<Electron, Proton, Neutron>;
 
-template< typename T >
-void print_if( Particle const & p )
- {
-  auto * ptr = std::get_if<T>(&p) ;
-  if (ptr) ptr->print() ;
- }
+template <typename T> void print_if(Particle const &p) {
+  auto *ptr = std::get_if<T>(&p);
+  if (ptr)
+    ptr->print();
+}
 
-int main()
- {
-  std::vector<Particle> ps =
-   { Electron(), Proton(), Neutron() } ;
+int main() {
+  std::vector<Particle> ps = {Electron(), Proton(), Neutron()};
 
-  for ( auto p : ps )
-   {
-    print_if<Electron>(p) ;
-    print_if<Proton>(p) ;
-    print_if<Neutron>(p) ;
-   }
- }
+  for (auto p : ps) {
+    print_if<Electron>(p);
+    print_if<Proton>(p);
+    print_if<Neutron>(p);
+  }
+}
