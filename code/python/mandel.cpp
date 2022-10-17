a8cedbde8e9da3152413642e4ac790d2fd185551
Sebastien Ponce <sebastien.ponce@cern.ch>
2022-10-17 12:26:33 +0200

    Made Complex.hpp up to date in python example

--- code/python/mandel.cpp
+++ code/python/mandel.cpp
@@ -1,10 +1,10 @@
 #include "mandel.hpp"
 
 int mandel(const Complex &a) {
-  Complex z = 0;
+  Complex z{0, 0};
   for (int n = 1; n < 100; n++) {
     z = z*z + a;
-    if (Complex(2) < z) {
+    if (Complex{2, 0} < z) {
       return n;
     }
   }
