f89d3924127cec9715b603bf5024b9398d0b5b17
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2023-02-14 12:38:36 +0100

    Improve race exercise.
    The race exercise races very infrequently on some platforms. Therefore:
    - Provide a variable to spawn more threads
    - Provide a run script that invokes the executable until a race occurs
    - Add thread sanitizer and valgrind as optional steps to the instructions
    
    Fix #353.

--- code/race/racing.cpp
+++ code/race/racing.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 /*
- * This program tries to increment an integer 200 times in two threads.
- * Check whether the result is indeed always 200.
+ * This program tries to increment an integer 100 times from multiple threads.
+ * If the result comes out at 100*nThread, it stays silent, but it will print
+ * an error if a race condition is detected.
+ * If you don't see it racing, try ./run ./racing, which keeps invoking the
+ * executable until a race condition is detected.
  */
+
+constexpr unsigned int nThread = 2;
 
 int main() {
   int nError = 0;
@@ -19,20 +25,17 @@ int main() {
       }
     };
 
-    // Run with two threads
-    std::thread t1(inc100);
-    std::thread t2(inc100);
-    for (auto t : {&t1,&t2}) t->join();
+    // Start up all threads:
+    std::vector<std::thread> threads;
+    for (unsigned int i = 0; i < nThread; ++i) threads.emplace_back(inc100);
+    for (auto & thread : threads) thread.join();
 
     // Check
-    if (a != 200) {
-      std::cout << "Race: " << a << ' ';
+    if (a != nThread * 100) {
+      std::cerr << "Race detected! Result: " << a << '\n';
       nError++;
-    } else {
-      std::cout << '.';
     }
   }
-  std::cout << '\n';
 
   return nError;
 }
--- code/race/solution/racing.sol.cpp
+++ code/race/solution/racing.sol.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 #include <mutex>
 
 /*
- * This program tries to increment an integer 200 times in two threads.
- * We fix the race condition by locking a mutex before each increment.
+ * This program tries to increment an integer 100 times from multiple threads.
+ * If the result comes out at 100*nThread, it stays silent, but it will print
+ * an error if a race condition is detected.
+ * To run it in a loop, use
+ *   ./run ./racing.sol
  */
+
+constexpr unsigned int nThread = 2;
 
 int main() {
   int nError = 0;
@@ -22,20 +28,17 @@ int main() {
       }
     };
 
-    // Run with two threads
-    std::thread t1(inc100);
-    std::thread t2(inc100);
-    for (auto t : {&t1,&t2}) t->join();
+    // Start up all threads:
+    std::vector<std::thread> threads;
+    for (unsigned int i = 0; i < nThread; ++i) threads.emplace_back(inc100);
+    for (auto & thread : threads) thread.join();
 
     // Check
-    if (a != 200) {
-      std::cout << "Race: " << a << ' ';
+    if (a != nThread * 100) {
+      std::cerr << "Race detected! Result: " << a << '\n';
       nError++;
-    } else {
-      std::cout << '.';
     }
   }
-  std::cout << '\n';
 
   return nError;
 }
