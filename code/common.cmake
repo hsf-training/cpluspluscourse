9d9482559786a3658d9a665a3c58ebd11c4d5c84
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-03-08 16:43:41 +0100

    Simplify cmake setup
    * Merge SolutionTarget.cmake and CompilerSettings.cmake into single file
    * Require C++ standard

--- code/common.cmake
+++ code/common.cmake
@@ -1,19 +1,12 @@
-#
-# Small module used in every project to set up the default "compilation
-# environment".
-#
-
 # Guard this file against multiple inclusions.
-get_property( _compilersSet GLOBAL PROPERTY COMPILER_SETTINGS_DONE SET )
-if( _compilersSet )
-   unset( _compilersSet )
+get_property( _common GLOBAL PROPERTY COMMON_DONE SET )
+if( _common )
+   unset( _common )
    return()
 endif()
-set_property( GLOBAL PROPERTY COMPILER_SETTINGS_DONE TRUE )
+set_property( GLOBAL PROPERTY COMMON_DONE TRUE )
 
-
-# Set up a Debug build type by default, if the user didn't ask for something
-# else.
+# Set up a Debug build type by default, if none was specified
 if( NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES )
    set( CMAKE_BUILD_TYPE "Debug" CACHE
         STRING "Choose the type of build." FORCE )
@@ -23,6 +16,7 @@ endif()
 
 # Use C++17 in the project by default, or as high of a value as possible.
 set( CMAKE_CXX_STANDARD 17 CACHE STRING "C++ standard to use" )
+set( CMAKE_CXX_STANDARD_REQUIRED ON )
 set( CMAKE_CXX_EXTENSIONS FALSE CACHE BOOL "(Dis)Allow C++ extensions" )
 
 # Enable (almost) all warnings for the build.
@@ -31,4 +25,9 @@ if( ( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" ) OR
    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra" )
 elseif( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC" )
    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4" )
+endif()
+
+# Trivial module to set up the "solution" target for all projects.
+if( NOT TARGET solution )
+   add_custom_target( solution )
 endif()
