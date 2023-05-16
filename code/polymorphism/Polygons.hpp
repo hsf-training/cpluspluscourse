9015d25e6e17ba1b8883465584db675b4a2f4c24
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-05-16 15:48:31 -0400

    Rename Polygon to RegularPolygon

--- code/polymorphism/Polygons.hpp
+++ code/polymorphism/Polygons.hpp
@@ -1,20 +1,20 @@
 #pragma once
 
-class Polygon {
+class RegularPolygon {
 public:
-    Polygon(int n, float radius);
+    RegularPolygon(int n, float radius);
     float computePerimeter() const;
 protected:
     int m_nbSides;
     float m_radius;
 };
 
-class Pentagon : public Polygon {
+class Pentagon : public RegularPolygon {
 public:
     Pentagon(float radius);
 };
 
-class Hexagon : public Polygon {
+class Hexagon : public RegularPolygon {
 public:
     Hexagon(float radius);
     // 6*radius is easier than generic case
