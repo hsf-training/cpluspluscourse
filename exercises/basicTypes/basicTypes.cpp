c975b5840ef0d2e9709320b3f9adf3dd4d782f31
Kateryna <kataokopnaa@gmail.com>
2025-02-05 11:23:36 +0200

    basicTypes ex. - solved

--- exercises/basicTypes/basicTypes.cpp
+++ exercises/basicTypes/basicTypes.cpp
@@ -15,19 +15,19 @@
 int main() {
   std::cout << "Using literals of different number types:\n";
   print(5);
-  print(5/2);           //FIXME
+  print(5/2.);           
   print(100/2ull);
   print(2 + 4ull);
   print(2.f + 4ull);
-  print(0u - 1u);       // FIXME
-  print(1.0000000001f); // FIXME Why is this number not represented correctly?
-  print(1. + 1.E-18);   // FIXME
+  print(0-1);       
+  print(1.0000000001); //Why is this number not represented correctly?
+  print(1.l + 1E-18);   //
 
   std::cout << "\nUsing increment and decrement operators:\n";
   int a = 1;
   int b;
   int c;
-  print(b = a++);       // Q: What is the difference between a++ and ++a?
+  print(b = a++);       // c?
   print(c = ++a);
   print(a);
   print(b);
